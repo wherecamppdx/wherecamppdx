f15c809a0ff21bf877ac213ac34c52f7543fe9bc
Reid Beels <mail@reidbeels.com>
2013-08-10 22:20:20 -0700

    Add Stumptown Syndicate callout with other event links

--- footer.php
+++ footer.php
@@ -1,8 +1,24 @@
 
-	<div id="footer">
-		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span>
-		<span class="meta-sep">|</span>
-		<span id="theme-link"><a href="http://www.plaintxt.org/themes/sandbox/" title="<?php _e( 'Sandbox theme for WordPress', 'sandbox' ) ?>" rel="designer"><?php _e( 'Sandbox', 'sandbox' ) ?></a></span>
+	<div id="footer" class="cf">
+    <div id="syndicate-callout">
+      <h3>Presented by</h3>
+      <a href="http://stumptownsyndicate.org">
+        <img alt="Stumptown Syndicate" src="<?php bloginfo('template_directory'); ?>/i/syndicate_logo.png" width="140">
+      </a>
+      <p>Creating resilient, radically inclusive tech and maker communities that empower positive change.</p>
+    </div>
+    <div id="other-events">
+      <h3>Other events</h3>
+      <a href="http://opensourcebridge.org" class="osbridge">
+        <img alt="Open Source Bridge" src="<?php bloginfo('template_directory'); ?>/i/osbridge_logo.png" width="140">
+      </a>
+      <a href="http://igniteportland.org">
+        <img alt="Ignite Portland" src="<?php bloginfo('template_directory'); ?>/i/ignite_logo.png" width="140">
+      </a>
+      <a href="http://barcampportland.org">
+        <img alt="BarCamp Portland" src="<?php bloginfo('template_directory'); ?>/i/barcamp_logo.png" width="140">
+      </a>
+    </div>
 	</div><!-- #footer -->
 
 </div><!-- #wrapper .hfeed -->
